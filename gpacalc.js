1862b6b5e59368e587aa85f2b8cadb540d3c65f5
quuuut <liujonathan21@gmail.com>
2025-09-22 15:05:17 +0930

    a

--- gpacalc.js
+++ gpacalc.js
@@ -1,3 +1,1 @@
-DMU.toast({message: "This script will no longer work, please move to the new version. You will be redirected after a few seconds.", toastStyle: "error", onClose: () => {
-  window.location.href = "https://quuuut.github.io/gpacalc?from=bookmarklet"
-}});
+LoopToast.showError("Deprecation", "This script will no longer work, please move to the new version. You will be redirected after a few seconds.");setTimeout(() => {window.location.href = "https://quuuut.github.io/gpacalc?from=bookmarklet"}, 3000);
